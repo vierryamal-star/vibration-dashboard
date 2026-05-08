858620dcb9f8cd71307d221a57b0ae81de43e7c4
vierryamal-star <vierryamal@gmail.com>
2026-05-08 13:51:27 +0700

    Update 1_Trend.py

--- pages/1_Trend.py
+++ pages/1_Trend.py
@@ -57,7 +57,7 @@ if mode == "Detail satu equipment":
     with c3:
         sel_dir = st.multiselect("Direction", ["H","V","A"], default=["H","V","A"])
 
-   df_tr = df_hist[
+  df_tr = df_hist[
     (df_hist["equipment"] == sel_eq) &
     (df_hist["titik"] == sel_titik) &
     (df_hist["direction"].isin(sel_dir))
